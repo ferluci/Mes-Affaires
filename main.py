1608262f4cb7e73348cffc46777bfbae6428450e
Konstantin Anischenko <anishenko00@gmail.com>
2017-07-09 12:31:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 
 '''
-Copyright (c) 2017 Anischenko Konstantin Maximovich
+Copyright (c) 2016 Anischenko Konstantin Maximovich
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
